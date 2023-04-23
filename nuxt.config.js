695b7e02349c531e0679bb5dff7cb723350e8999
Michiel Gerritsen <michiel@controlaltdelete.nl>
2023-04-23 21:27:33 +0200

    Changed page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'magento2-model-generator',
+    title: 'Magento 2 Model Generator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
